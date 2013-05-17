da7b9e4c166c435abbc31f6bfefb89a722ef5b60
JP Slavinsky <jpslav@gmail.com>
2013-05-17 14:31:33 -0700

    added more comments

--- app/features/request_registration.rb
+++ app/features/request_registration.rb
@@ -17,24 +17,37 @@ class RequestRegistration
 
   # A generic authorization method that could be used by any code (e.g.
   # controllers) that call this feature
+
   def can_be_performed_by?(user)
     user == @user
   end
 
   def execute
-    # Run validations 
+    # Run validations (see note below class name above)
+    
     return if !self.valid?
 
-    # Create pending student
+    # Create pending student; if we want all AR code out of all features, what
+    # about just saying .new here and having the calling code (i.e. the controller)
+    # save it.  We'd either have to pass back the student or pass back an object
+    # of pending AR calls that need to be done.
+
     student = Student.create(user: @user, section: @section, is_auditing: @is_auditing)
 
-    # Approve if preapproved
+    # Approve if preapproved; the feature approach seems to get rid of a lot of
+    # ActiveRecord callbacks and stuff like observers;  everything just becomes
+    # a step in the execute method
+
     if !@is_auditing && klass.is_preapproved?(@user) && (!klass.ended? || !Rails.env.production?)
       ApprovePendingStudent.new(student).execute
     else
       StudentMailer.registration_pending(student).deliver  # likely a notification engine call instead
     end
   end
+
+  # Having this method gives us an easy way to stub access to the klass
+  # alternatively, the section stub could have a klass stub, e.g.
+  # section = mock_model("Section", :klass => mock_model("Klass"))
 
   def klass
     @section.klass
